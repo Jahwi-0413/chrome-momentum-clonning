71a6ed2a5e19bc8a724535afd60ebd79b89e911c
jaehe <jaehe0413@naver.com>
2022-09-03 22:56:33 +0900

    complete weather

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "1432af0789b31c44b0161097d79ee363"
+
+function onGeoOk(position){     //position -> GeolocationPosition obj
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then(response=> response.json()
+    .then(data => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    }));
+}
+
+function onGeoError(){
+  alert("Can't find you. no weahter for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
