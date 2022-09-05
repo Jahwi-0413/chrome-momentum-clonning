6cd9535d9db6fb922f736a2d4ba78be9521f524e
jaehe <jaehe0413@naver.com>
2022-09-05 17:58:24 +0900

    add weather css

--- js/weather.js
+++ js/weather.js
@@ -10,7 +10,7 @@ function onGeoOk(position){     //position -> GeolocationPosition obj
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main.toLowerCase()} / ${data.main.temp} °C`;
     }));
 }
 
