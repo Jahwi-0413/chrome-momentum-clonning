34104d4baa058db9fe80673cf9eb86d03e6595a5
jaehe <jaehe0413@naver.com>
2022-09-06 11:47:06 +0900

    add weather async-await

--- js/weather.js
+++ js/weather.js
@@ -1,17 +1,18 @@
 const API_KEY = "1432af0789b31c44b0161097d79ee363"
 
-function onGeoOk(position){     //position -> GeolocationPosition obj
+async function onGeoOk(position){     //position -> GeolocationPosition obj
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  fetch(url)
-    .then(response=> response.json()
-    .then(data => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
-      city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main.toLowerCase()} / ${data.main.temp} °C`;
-    }));
+
+  const response = await fetch(url);
+  const data = await response.json();
+  
+  const weather = document.querySelector("#weather span:first-child");
+  const city = document.querySelector("#weather span:last-child");
+  
+  weather.innerText = `${data.weather[0].main.toLowerCase()} / ${data.main.temp} °C`;
+  city.innerText = data.name;
 }
 
 function onGeoError(){
